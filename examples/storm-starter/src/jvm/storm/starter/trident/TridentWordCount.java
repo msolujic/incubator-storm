f81a1f80ee34264b4f804552983eb2c2c2126da7
msolujic <milos.solujic@gmail.com>
2014-06-24 17:11:45 +0200

    cleaned up code and added cluster shutdown

--- examples/storm-starter/src/jvm/storm/starter/trident/TridentWordCount.java
+++ examples/storm-starter/src/jvm/storm/starter/trident/TridentWordCount.java
@@ -38,8 +38,12 @@ import storm.trident.tuple.TridentTuple;
 
 
 public class TridentWordCount {
+	
   public static class Split extends BaseFunction {
-    @Override
+
+	private static final long serialVersionUID = -273290171088975803L;
+
+	@Override
     public void execute(TridentTuple tuple, TridentCollector collector) {
       String sentence = tuple.getString(0);
       for (String word : sentence.split(" ")) {
@@ -48,20 +52,27 @@ public class TridentWordCount {
     }
   }
 
+  @SuppressWarnings("unchecked")
   public static StormTopology buildTopology(LocalDRPC drpc) {
-    FixedBatchSpout spout = new FixedBatchSpout(new Fields("sentence"), 3, new Values("the cow jumped over the moon"),
-        new Values("the man went to the store and bought some candy"), new Values("four score and seven years ago"),
-        new Values("how many apples can you eat"), new Values("to be or not to be the person"));
+    FixedBatchSpout spout = new FixedBatchSpout(new Fields("sentence"), 3, 
+    	new Values("the cow jumped over the moon"),
+        new Values("the man went to the store and bought some candy"), 
+        new Values("four score and seven years ago"),
+        new Values("how many apples can you eat"), 
+        new Values("to be or not to be the person"));
     spout.setCycle(true);
 
     TridentTopology topology = new TridentTopology();
-    TridentState wordCounts = topology.newStream("spout1", spout).parallelismHint(16).each(new Fields("sentence"),
-        new Split(), new Fields("word")).groupBy(new Fields("word")).persistentAggregate(new MemoryMapState.Factory(),
-        new Count(), new Fields("count")).parallelismHint(16);
+    TridentState wordCounts = topology.newStream("spout1", spout).parallelismHint(16)
+    	.each(new Fields("sentence"), new Split(), new Fields("word"))
+        .groupBy(new Fields("word"))
+        .persistentAggregate(new MemoryMapState.Factory(), new Count(), new Fields("count")).parallelismHint(16);
 
-    topology.newDRPCStream("words", drpc).each(new Fields("args"), new Split(), new Fields("word")).groupBy(new Fields(
-        "word")).stateQuery(wordCounts, new Fields("word"), new MapGet(), new Fields("count")).each(new Fields("count"),
-        new FilterNull()).aggregate(new Fields("count"), new Sum(), new Fields("sum"));
+    topology.newDRPCStream("words", drpc)
+    	.each(new Fields("args"), new Split(), new Fields("word")).groupBy(new Fields("word"))
+    	.stateQuery(wordCounts, new Fields("word"), new MapGet(), new Fields("count"))
+    	.each(new Fields("count"), new FilterNull())
+    	.aggregate(new Fields("count"), new Sum(), new Fields("sum"));
     return topology.build();
   }
 
@@ -72,10 +83,13 @@ public class TridentWordCount {
       LocalDRPC drpc = new LocalDRPC();
       LocalCluster cluster = new LocalCluster();
       cluster.submitTopology("wordCounter", conf, buildTopology(drpc));
-      for (int i = 0; i < 100; i++) {
+      Thread.sleep(5000);
+      for (int i = 0; i < 10; i++) {
         System.out.println("DRPC RESULT: " + drpc.execute("words", "cat the dog jumped"));
         Thread.sleep(1000);
       }
+      cluster.shutdown();
+      drpc.shutdown();
     }
     else {
       conf.setNumWorkers(3);
